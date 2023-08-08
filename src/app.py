b320c356e7117b82ffb8ab6c9ab539d7db827022
aronweiler <aronweiler@gmail.com>
2023-08-08 16:09:37 -0700

    Some more testing

--- src/app.py
+++ src/app.py
@@ -1,6 +1,17 @@
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+github_output = os.environ.get("GITHUB_OUTPUT", "not available")
+changes = os.environ.get("CHANGES", "None found")
 
 output = "Hello World!  Fake summary ;)"
+
+logging.debug(output)
+logging.debug("GH Output: " + github_output)
+logging.debug("Changes: " + changes)
+
 
 # Write the output to the $GITHUB_OUTPUT environment file
 # See: https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action?learn=create_actions&learnProduct=actions#writing-the-action-code
