a7aa563a3d7b0349254bd60c080eb12b88314c6d
aronweiler <aronweiler@gmail.com>
2023-08-09 22:45:40 -0700

    starting refactor for adding refactor hah

--- src/integrations/commenter_base.py
+++ src/integrations/commenter_base.py
@@ -1,6 +1,6 @@
 from typing import List
 from abc import ABC, abstractmethod
-from code_review_tool import CodeComment
+from code_comment import CodeComment
 
 class CommenterBase(ABC):
     @abstractmethod
