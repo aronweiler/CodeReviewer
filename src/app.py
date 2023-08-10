722cc109af2ea341f8e41a3aaed99309595c0962
aronweiler <aronweiler@gmail.com>
2023-08-09 19:09:15 -0700

    Launch related stuff

--- src/app.py
+++ src/app.py
@@ -1,20 +1,63 @@
 import os
 import logging
+import sys
+import argparse
 
-logging.basicConfig(level=logging.DEBUG)
+from integrations.github_commenter import GitHubCommenter
+from code_reviewer import CodeReviewer
+from review_configuration import ReviewConfiguration
 
-github_output = os.environ.get("GITHUB_OUTPUT", "not available")
-changes = os.environ.get("CHANGES", "None found")
-
-output = "Hello World!  Fake summary ;)"
-
-logging.debug(output)
-logging.debug("GH Output: " + github_output)
-logging.debug("Changes: " + changes)
+PROVIDERS = {"github": GitHubCommenter(), "gitlab": None, "bitbucket": None}
 
 
-# Write the output to the $GITHUB_OUTPUT environment file
-# See: https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action?learn=create_actions&learnProduct=actions#writing-the-action-code
-# with open(os.environ["GITHUB_OUTPUT"], "a") as f:
-#     f.write(f"summary={output}")
+class ReviewRunner:
+    def __init__(self):
+        self.parse_arguments()
+        #self.set_logging_level()
 
+    def parse_arguments(self):
+        # Add arguments
+        parser = argparse.ArgumentParser()
+        
+        parser.add_argument("file_paths", nargs="+", help="List of file paths.")
+
+        self.args = parser.parse_args()
+
+    def set_logging_level(self):
+        # Set the logging level
+        numeric_level = getattr(logging, self.args.logging_level.upper(), None)
+        logging.basicConfig(level=numeric_level)
+        logging.info("Started logging")
+
+    def main(self):
+        # Get the paths to the source code files
+        paths = self.args.file_paths
+        logging.debug("Paths: " + str(paths))
+
+        configuration = ReviewConfiguration.from_environment()
+
+        # Get the source code files
+        source_code_files = []
+        for path in paths:
+            if os.path.isfile(path):
+                source_code_files.append(path)
+            else:
+                logging.warning(f"Path does not exist: {path}")
+
+        logging.debug("Source code files: " + str(source_code_files))
+
+        # Run the code review
+        code_comments = self.do_review(source_code_files, configuration)
+
+        # Have the provider-specific code add the review to the PR
+        PROVIDERS[configuration.provider].add_comments(code_comments)
+
+
+    def do_review(self, source_code_files, configuration: ReviewConfiguration):        
+        code_reviewer = CodeReviewer(configuration)
+        review = code_reviewer.review(source_code_files)
+        return review
+
+
+if __name__ == "__main__":
+    ReviewRunner().main()
