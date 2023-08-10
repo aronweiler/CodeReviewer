4172ff3af3e61deaaee349c73929ecdeb6537df9
aronweiler <aronweiler@gmail.com>
2023-08-09 21:29:31 -0700

    Code cleanup based on GPT comments

--- src/integrations/commenter_base.py
+++ src/integrations/commenter_base.py
@@ -1,7 +1,8 @@
+from typing import List
 from abc import ABC, abstractmethod
 from code_review_tool import CodeComment
 
 class CommenterBase(ABC):
     @abstractmethod
-    def add_comments(self, comment:CodeComment):
+    def add_comments(self, comments: List[CodeComment]):
         pass
--- src/review_configuration.py
+++ src/review_configuration.py
@@ -1,6 +1,11 @@
 import os
+import logging
+import json
 from llm_arguments import LLMArguments
+from integrations.github_commenter import GitHubCommenter
+from integrations.file_commenter import FileCommenter
 
+PROVIDERS = {"github": GitHubCommenter(), "gitlab": None, "bitbucket": None, "file": FileCommenter()}
 
 class ReviewConfiguration:
     def __init__(self, provider, include_summary, llm_arguments) -> None:
@@ -10,17 +15,24 @@ class ReviewConfiguration:
 
     @staticmethod
     def from_json_file(file_path: str):
-        import json
-
-        with open(file_path, "r") as f:
-            config = json.load(f)
+        try:
+            with open(file_path, "r") as f:
+                config = json.load(f)
+        except FileNotFoundError:
+            logging.warn(f"Could not find configuration file at {file_path}, defaults will be used.")        
+            return ReviewConfiguration.from_dict({})
 
         return ReviewConfiguration.from_dict(config)
 
     @staticmethod
     def from_environment():
-        provider = os.environ.get("CR_PROVIDER", False)
-        include_summary = os.environ.get("CR_INCLUDE_SUMMARY", "False").lower() == "true"
+        
+        provider = os.environ.get("CR_PROVIDER", "file")
+        if provider.lower() not in PROVIDERS:
+            logging.error(f"Provider {provider} is not supported.")
+            raise ValueError(f"Provider {provider} is not supported.")
+
+        include_summary = os.environ.get("CR_INCLUDE_SUMMARY", "false").lower() == "true"
 
         llm_arguments = LLMArguments.from_environment()
 
@@ -29,8 +41,17 @@ class ReviewConfiguration:
     @staticmethod
     def from_dict(config: dict):
         provider = config.get("provider", None)        
+        if provider.lower() not in PROVIDERS:
+            logging.error(f"Provider {provider} is not supported.")
+            raise ValueError(f"Provider {provider} is not supported.")
+        
         include_summary = config.get("include_summary", False)
 
-        llm_arguments = LLMArguments.from_dict(config.get("llm", None))
+        llm_node = config.get("llm", None)
+        if llm_node is None:
+            logging.warn("No LLM configuration found, using defaults.")
+            llm_node = {}
+
+        llm_arguments = LLMArguments.from_dict(llm_node)
 
         return ReviewConfiguration(provider, include_summary, llm_arguments)
