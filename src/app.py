5ff3e359b2df5d24df17f2224e69fc5d4e760b25
aronweiler <aronweiler@gmail.com>
2023-08-12 12:08:26 -0700

    Updated github action

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 import os
 import logging
-import argparse
 from typing import List
 
 from review.code_reviewer import CodeReviewer
@@ -13,27 +12,29 @@ VALID_TYPES = ["review", "refactor", "document"]
 
 class ReviewRunner:
     def __init__(self):
-        self.parse_arguments()
+        self.load_arguments()
         self.set_logging_level()
         self.configuration = CodeReviewerConfiguration.from_environment()
 
     def main(self):
         # Lowercase the type to ensure case-insensitive comparison
-        operation_type = self.args.type.lower()
+        operation_type = self.cr_type.lower()
         if operation_type == "review":
             self.do_code_review()
         elif operation_type == "refactor":
             self.do_code_refactor()
         elif operation_type == "document":
             self.do_code_documentation()
+        else:
+            raise ValueError(f"Invalid type: {self.cr_type}")
 
     def do_code_refactor(self):
         # Get the source code files
         source_code_files = self.get_source_code_files()
 
         # Get the source and target branches from arguments
-        source_branch = self.args.source_branch
-        target_branch = self.args.target_branch
+        source_branch = self.source_branch
+        target_branch = self.target_branch
 
         # Initialize the CodeRefactor class and run the refactor
         code_refactor = CodeRefactor(self.configuration)
@@ -66,55 +67,56 @@ class ReviewRunner:
 
     def get_source_code_files(self) -> List[str]:
         # Get the paths to the source code files from arguments
-        paths = self.args.file_paths
+        paths = self.target_files
+        if paths is None or len(paths) == 0:
+            logging.info("No paths specified, using all files in repo")
+
         logging.debug("Code Refactor Paths: " + str(paths))
 
         # Check if each path is a file and add it to the source code files list
         source_code_files = [path for path in paths if os.path.isfile(path)]
-        for path in paths:
-            if not os.path.isfile(path):
-                logging.warning(f"Path does not exist: {path}")
+        source_code_directories = [path for path in paths if os.path.isdir(path)]
+        
+        # If a directory is specified, add all files in the directory to the source code files list
+        for directory in source_code_directories:
+            for root, dirs, files in os.walk(directory):
+                for file in files:
+                    source_code_files.append(os.path.join(root, file))
 
         logging.debug("Source code files: " + str(source_code_files))
 
         return source_code_files
 
-    def parse_arguments(self):
-        # Initialize the argument parser and add arguments
-        parser = argparse.ArgumentParser()
-        parser.add_argument(
-            "--logging-level", default="INFO", help="Logging level. Default: INFO"
-        )
-        parser.add_argument(
-            "--type",
-            required=True,
-            help=f"Type of run. Choices are: {str(VALID_TYPES)}",
-        )
-        parser.add_argument(
-            "--source_branch",
-            default="main",
-            help="Source branch to pull from. Default: main",
-        )
-        parser.add_argument(
-            "--target_branch",
-            default="code-refactor",
-            help="Target branch to push to. Default: code-refactor",
-        )
-        parser.add_argument("file_paths", nargs="+", help="List of file paths.")
+    def load_arguments(self):
+        # Load the arguments from the environment
 
-        self.args = parser.parse_args()
+        # These are set in the LLMConfiguration class
+        # self.model = os.getenv("CR_MODEL", "gpt-3.5-turbo-0613")
+        # self.temperature = os.getenv("CR_TEMPERATURE", "0.0")
+        # self.max_supported_tokens = os.getenv("CR_MAX_SUPPORTED_TOKENS", "4096")
+        # self.max_completion_tokens = os.getenv("CR_MAX_COMPLETION_TOKENS", "2048")
+
+        self.log_level = os.getenv("CR_LOG_LEVEL", "info")
+        self.source_branch = os.getenv("CR_SOURCE_BRANCH", "main")
+        self.target_branch = os.getenv("CR_TARGET_BRANCH", "test-branch")
+        self.cr_type = os.getenv("CR_TYPE", None)
+        self.target_files = os.getenv("CR_TARGET_FILES", "")
+
+        # Split the target files string into a list
+        self.target_files = self.target_files.split(",")
+        logging.debug("Target files: " + str(self.target_files))
 
         # Validate that the type argument is one of the valid choices
-        if self.args.type.lower() not in VALID_TYPES:
+        if self.cr_type.lower() not in VALID_TYPES:
             raise ValueError(
-                f"Invalid type: {self.args.type}. Valid types are: {VALID_TYPES}"
+                f"Invalid type: {self.cr_type}. Valid types are: {VALID_TYPES}"
             )
 
     def set_logging_level(self):
         # Get the numeric value of the logging level and set it
-        numeric_level = getattr(logging, self.args.logging_level.upper(), None)
+        numeric_level = getattr(logging, self.log_level.upper(), None)
         if not isinstance(numeric_level, int):
-            raise ValueError(f"Invalid log level: {self.args.logging_level}")
+            raise ValueError(f"Invalid log level: {self.log_level}")
         logging.basicConfig(level=numeric_level)
         logging.info("Started logging")
 
